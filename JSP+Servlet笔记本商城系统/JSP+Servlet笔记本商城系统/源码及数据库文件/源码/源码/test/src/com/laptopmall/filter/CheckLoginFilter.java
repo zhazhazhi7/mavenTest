a3f533e15a4dc552133d516bc018aef6309e8826
zhazhazhi <2055418639@qq.com>
2023-06-12 22:38:15 +0800

    test

---
+++ JSP+Servlet笔记本商城系统/JSP+Servlet笔记本商城系统/源码及数据库文件/源码/源码/test/src/com/laptopmall/filter/CheckLoginFilter.java
@@ -0,0 +1,49 @@
+package com.laptopmall.filter;
+
+import com.laptopmall.bean.User;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * 	全局拦截器，用户没登录时，强制跳转到login.jsp
+ * @author chen
+ *
+ */
+@WebFilter(filterName="CheckLoginFilter",urlPatterns="/*")
+public class CheckLoginFilter implements Filter{
+
+	private static final String[] ALLOWED_PATHS = {"/login","/login.jsp", "/register", "/register.jsp","/js/jquery-3.3.1.min.js","/js/bootstrap.min.js","/css/bootstrap.css"};
+	
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpSession session = req.getSession();
+		String path = req.getServletPath();
+		System.out.println(path);
+		boolean allowed = Arrays.asList(ALLOWED_PATHS).contains(path);
+		User user = (User)session.getAttribute("CURRENT_USER");
+		if(!allowed && user == null) {
+			((HttpServletResponse)response).sendRedirect("/com/laptopmall/login.jsp");
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		
+	}
+
+}
---
+++ JSP+Servlet笔记本商城系统/JSP+Servlet笔记本商城系统/源码及数据库文件/源码/源码/test/src/com/laptopmall/util/JdbcUtil.java
@@ -0,0 +1,55 @@
+package com.laptopmall.util;
+
+import java.sql.*;
+
+public class JdbcUtil {
+
+	private static String url = "jdbc:mysql://localhost:3306/laptopmall";
+	private static String username = "root";
+	private static String password = "123456";
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+
+	public static void close(Connection conn, PreparedStatement ps, ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (conn != null) {
+						conn.close();
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
